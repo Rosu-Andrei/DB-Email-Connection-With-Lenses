555d34ff8169290917d4c5c9d62b7476bb2575b4
ivonaracasean <ivona.racasean@hcltech.com>
2024-12-09 11:11:24 +0200

    Interpreter abstraction

--- src/interpreter/interpreter.ts
+++ src/interpreter/interpreter.ts
@@ -2,16 +2,29 @@ import { Event } from "../events/events";
 import { eventProcessor } from "../events/events.processors";
 import { AppState } from "../App";
 
-export type Interpreter = (events: Event[]) => Promise<any>;
-
-export const interpreters: Record<string, Interpreter> = {
-    count: async (events) => events.length,
-
-    asJson: async (events) => {
-        if (!Array.isArray(events)) {
-            throw new Error('Invalid input: events must be an array');
-        }
-        const initialState: AppState = {connections: []};
-        return eventProcessor(events, initialState) as AppState;
-    }
+export type Interpreter<T> =  {
+    process: (es: Event[]) => Promise<T> // How the data is processed
+    render: (t: T) => string // How data is displayed
+    mimetype: string // MIME type for the result
 }
+  
+export const countProcessor: Interpreter<number> = {
+    process: async (events: Event[]) => events.length,
+    render: (count) => JSON.stringify({count}, null, 2),
+    mimetype: 'application/json'
+ }
+  
+export const jsonProcessor:Interpreter<any> = {
+    process: async (events: Event[]) => {
+        const initialState: AppState = { connections: [] };
+        return eventProcessor(events, initialState) as AppState;
+    },
+    render: (state) => JSON.stringify(state, null,2),
+    mimetype: 'application/json'
+ }
+  
+export const allInterpreters : Record<string, Interpreter<any>> = {
+   count: countProcessor,
+   asJson: jsonProcessor
+ }
+  
